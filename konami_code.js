84296fbe3715d4d40e61f22a848334f93db6a9d7
Jacob Berman <jacobberman1995@gmail.com>
2018-03-28 18:36:00 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,9 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-document.addEventListener('keydown', konamiCode(e));
+document.addEventListener('keydown', konamiCode (e) {
+  console.log(e.which);
+})
+
 }
 
-function konamiCode(e){
-  console.log(e.which);
-}
